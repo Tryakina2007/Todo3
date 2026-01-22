081ec923b2589888809e4ea082aa15d49404bbca
Регина Трякина <regina.tryakina.07@list.ru>
2026-01-22 09:19:37 +0300

    labbb5

---
+++ Todo3/DobavlenieZadach.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Todo3
+{
+    /// <summary>
+    /// Логика взаимодействия для DobavlenieZadach.xaml
+    /// </summary>
+    public partial class DobavlenieZadach : Window
+    {
+        public DobavlenieZadach()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Событие создано!");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
